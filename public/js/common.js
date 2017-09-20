2b28456944898065f9f7f2ab27d54e2931567688
QiLvNuanQingYou <523070160@qq.com>
2017-09-20 14:12:34 +0800

    完成登录和退出功能，提取出公共部分，PHP引入index

--- public/js/common.js
+++ public/js/common.js
@@ -1,8 +1,31 @@
+NProgress.start();
 
-	NProgress.start();
+NProgress.done();
 
-	NProgress.done();
-
-	$('.navs ul').prev('a').on('click', function () {
-		$(this).next().slideToggle();
-	});
+$('.navs ul').prev('a').on('click', function () {
+    $(this).next().slideToggle();
+});
+//实现退出功能
+$('#logoutBtn').click(function () {
+    $.ajax({
+        type: 'post',
+        url: '/api/logout',
+        dataType: 'json',
+        success: function (data) {
+            //console.log(data);
+            if (data.code == 200) {
+                location.href = '/main/login';
+            }
+        }
+    });
+});
+//验证用户是否登录了
+var flag = $.cookie('PHPSESSID');
+if (!flag) {
+    location.href = '/main/login';
+}
+//填充头像信息
+var loginInfo = $.cookie('loginInfo');
+loginInfo = loginInfo && JSON.parse(loginInfo);
+$('.aside .profile img').attr('src',loginInfo.tc_avatar);
+$('.aside .profile h4').html(loginInfo.tc_name);
