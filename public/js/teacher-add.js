7f50ea5abfdbed67d8e00eecf726a92388d7aa81
QiLvNuanQingYou <523070160@qq.com>
2017-09-22 20:47:24 +0800

    添加编辑讲师的简单提交功能

--- public/js/teacher-add.js
+++ public/js/teacher-add.js
@@ -1,7 +1,7 @@
-define(['jquery','template','util'],function($,template,util) {
+define(['jquery', 'template', 'util'], function ($, template, util) {
     //获取url中的参数
     var tcId = util.qs('tc_id');
-    if(tcId) {
+    if (tcId) {
         //编辑操作
         //console.log(tcId);
         $.ajax({
@@ -9,17 +9,37 @@ define(['jquery','template','util'],function($,template,util) {
             url: '/api/teacher/edit',
             data: {tc_id: tcId},
             dataType: 'json',
-            success: function(data) {
+            success: function (data) {
                 //console.log(data);
                 //用这种方式将文字信息挂载到数据上，就不用模板里再去判断了，比较省劲
                 data.result.operate = '编辑讲师'
-                var html = template('teacherTpl',data.result);
+                var html = template('teacherTpl', data.result);
                 $('#teacherInfo').html(html);
+                submitForm('/api/teacher/update');
             }
         });
     } else {
         //添加操作
-        var html = template('teacherTpl',{operate:'添加讲师'});
+        var html = template('teacherTpl', {operate: '添加讲师'});
         $('#teacherInfo').html(html);
+        submitForm('/api/teacher/add');
+    }
+    //表单提交函数
+    function submitForm(url) {
+        $('#teacherBtn').click(function () {
+            $.ajax({
+                type: 'post',
+                url: url,
+                data: $('#teacherForm').serialize(),
+                dataType: 'json',
+                success: function (data) {
+                    //console.log(data);
+                    if (data.code == 200) {
+                        location.href = '/teacher/teacher_list';
+                    }
+                }
+            });
+            console.log($('#teacherForm').serialize());
+        });
     }
 });
