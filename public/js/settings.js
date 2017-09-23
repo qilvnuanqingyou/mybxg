f4372062940c4867692f457ff1f066844ae37166
QiLvNuanQingYou <523070160@qq.com>
2017-09-23 20:03:20 +0800

    头像上传flash插件

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,4 +1,4 @@
-define(['jquery','template'],function($,template) {
+define(['jquery','template','uploadify'],function($,template) {
     $.ajax({
         type: 'get',
         url: '/api/teacher/profile',
@@ -7,6 +7,20 @@ define(['jquery','template'],function($,template) {
             //console.log(data);
             var html = template('settingsTpl',data.result);
             $('#settingsInfo').html(html);
+            //处理头像上传
+            $('#upfile').uploadify({
+                width : 120,
+                height : 120,
+                buttonText : '',
+                itemTemplate : '<span></span>',
+                swf : '/public/assets/uploadify/uploadify.swf',
+                uploader : '/api/uploader/avatar',
+                fileObjName : 'tc_avatar',
+                onUploadSuccess : function(a,b){
+                    var obj = JSON.parse(b);
+                    $('.preview img').attr('src',obj.result.path);
+                }
+            });
         }
     });
 });
