d7b8e32da12ff33c2fe9b2e9015e2c09408c1f49
QiLvNuanQingYou <523070160@qq.com>
2017-09-24 15:59:13 +0800

    完成个人中心表单提交

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,36 +1,64 @@
-define(['jquery','template','ckeditor','uploadify','region'],function($,template,CKEDITOR) {
+define(['jquery', 'template', 'ckeditor', 'uploadify', 'region', 'datepicker', 'language', 'validate', 'form'], function ($, template, CKEDITOR) {
     $.ajax({
         type: 'get',
         url: '/api/teacher/profile',
         dataType: 'json',
-        success: function(data) {
+        success: function (data) {
             //console.log(data);
-            var html = template('settingsTpl',data.result);
+            var html = template('settingsTpl', data.result);
             $('#settingsInfo').html(html);
             //处理头像上传
             $('#upfile').uploadify({
-                width : 120,
-                height : 120,
-                buttonText : '',
-                itemTemplate : '<span></span>',
-                swf : '/public/assets/uploadify/uploadify.swf',
-                uploader : '/api/uploader/avatar',
-                fileObjName : 'tc_avatar',
-                onUploadSuccess : function(a,b){
+                width: 120,
+                height: 120,
+                buttonText: '',
+                itemTemplate: '<span></span>',
+                swf: '/public/assets/uploadify/uploadify.swf',
+                uploader: '/api/uploader/avatar',
+                fileObjName: 'tc_avatar',
+                onUploadSuccess: function (a, b) {
                     var obj = JSON.parse(b);
-                    $('.preview img').attr('src',obj.result.path);
+                    $('.preview img').attr('src', obj.result.path);
                 }
             });
             // 处理省市县三级联动
             $('#pcd').region({
-                url : '/public/assets/jquery-region/region.json'
+                url: '/public/assets/jquery-region/region.json'
             });
             // 处理富文本
-            CKEDITOR.replace('editor',{
-                toolbarGroups : [
-                    { name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
-                    { name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] }
+            CKEDITOR.replace('editor', {
+                toolbarGroups: [
+                    {name: 'clipboard', groups: ['clipboard', 'undo']},
+                    {name: 'editing', groups: ['find', 'selection', 'spellchecker', 'editing']}
                 ]
+            });
+            //处理表单提交
+            $('#settingsForm').validate({
+                sendForm: false,
+                valid: function () {
+                    // 拼接籍贯信息
+                    var p = $('#p').find('option:selected').text();
+                    var c = $('#c').find('option:selected').text();
+                    var d = $('#d').find('option:selected').text();
+                    var hometown = p + '|' + c + '|' + d;
+
+                    // 更新富文本内容
+                    for(var instance in CKEDITOR.instances){
+                        CKEDITOR.instances[instance].updateElement();
+                    }
+                    $(this).ajaxSubmit({
+                        type: 'post',
+                        url: '/api/teacher/modify',
+                        data: {tc_hometown: hometown},
+                        dataType: 'json',
+                        success: function(data) {
+                            //console.log(data);
+                            if(data.code==200) {
+                                location.reload();
+                            }
+                        }
+                    });
+                }
             });
         }
     });
