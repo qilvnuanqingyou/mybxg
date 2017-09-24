b565d7157a279af922e001f02b031b93dfcb0871
QiLvNuanQingYou <523070160@qq.com>
2017-09-24 18:17:49 +0800

    完成基本信息填充

--- public/js/course-list.js
+++ public/js/course-list.js
@@ -1,13 +1,13 @@
-define(['jquery','template','util'],function($,template,util){
+define(['jquery', 'template', 'util'], function ($, template, util) {
     // 设置导航菜单选中
     util.setMenu(location.pathname);
     $.ajax({
         type: 'get',
         url: '/api/course',
         dataType: 'json',
-        success: function(data) {
+        success: function (data) {
             //console.log(data);
-            var html = template('courseTpl',{list : data.result});
+            var html = template('courseTpl', {list: data.result});
             $('#courseInfo').html(html);
         }
     });
