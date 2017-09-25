40569a5a1e54f10703ccf5829a7241622d2a774b
QiLvNuanQingYou <523070160@qq.com>
2017-09-25 16:43:25 +0800

    课程基本信息更新功能/基本信息的提交

--- public/js/course-basic.js
+++ public/js/course-basic.js
@@ -1,4 +1,4 @@
-define(['jquery','template','util'],function($,template,util){
+define(['jquery','template','util','validate','form'],function($,template,util){
     // 设置导航菜单选中
     util.setMenu('/course/course_add');
     // 获取课程ID
@@ -36,6 +36,23 @@ define(['jquery','template','util'],function($,template,util){
                         $('#secondType').html(html);
                     }
                 });
+            });
+            //处理表单提交
+            $('#basicForm').validate({
+                sendForm: false,
+                valid: function() {
+                    //提交表单
+                    $(this).ajaxSubmit({
+                        type: 'post',
+                        url: '/api/course/update/basic',
+                        data: {cs_id:csId},
+                        dataType: 'json',
+                        success: function(data) {
+                            //console.log(data);
+                            location.href = '/course/picture?cs_id' + data.result.csId;
+                        }
+                    });
+                }
             });
         }
     });
