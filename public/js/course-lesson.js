d7764aef81abfd44df95dd0313c1bae9552cad8e
QiLvNuanQingYou <523070160@qq.com>
2017-09-27 16:48:14 +0800

    完成添加课时和编辑课时的表单提交

--- public/js/course-lesson.js
+++ public/js/course-lesson.js
@@ -1,4 +1,4 @@
-define(['jquery','template','util','bootstrap'],function($,template,util){
+define(['jquery','template','util','bootstrap','form'],function($,template,util){
   // 设置导航菜单选中
   util.setMenu('/course/course_add');
   // 获取课程ID
@@ -16,9 +16,26 @@ define(['jquery','template','util','bootstrap'],function($,template,util){
       $('#lessonInfo').html(html);
       //处理添加课时操作
       $('#addLesson').click(function(){
-        var html = template('modalTpl',{operate : '添加课时'});
+        var html = template('modalTpl',{operate : '添加课时',ct_is_free: 0});
         $('#modalInfo').html(html);
         $('#chapterModal').modal();
+        // 处理添加课时的表单提交
+        $('#addOreditBtn').click(function(){
+          //console.log(1);
+          var is_free = $('#lessonForm .is_free')[0].checked ? 1 : 0;
+          $('#lessonForm').ajaxSubmit({
+            type : 'post',
+            url : '/api/course/chapter/add',
+            data : {ct_cs_id : csId,ct_is_free: is_free},
+            dataType : 'json',
+            success : function(data){
+              console.log(data);
+              if(data.code == 200){
+                location.reload();
+              }
+            }
+          });
+        });
       });
       //处理编辑课时操作
       $('.editLesson').click(function(){
@@ -36,6 +53,21 @@ define(['jquery','template','util','bootstrap'],function($,template,util){
             var html = template('modalTpl',data.result);
             $('#modalInfo').html(html);
             $('#chapterModal').modal();
+            // 处理编辑课时的表单提交
+            $('#addOreditBtn').click(function(){
+              var is_free = $('#lessonForm .is_free')[0].checked ? 1 : 0;
+              $('#lessonForm').ajaxSubmit({
+                type : 'post',
+                url : '/api/course/chapter/modify',
+                data : {ct_cs_id : csId,ct_id : ctId,ct_is_free: is_free},
+                dataType : 'json',
+                success : function(data){
+                  if(data.code == 200){
+                    location.reload();
+                  }
+                }
+              });
+            });
           }
         });
       });
