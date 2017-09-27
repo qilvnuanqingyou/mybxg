77947f897eb5cc00579c9489ad2fee22568f418e
QiLvNuanQingYou <523070160@qq.com>
2017-09-27 16:12:28 +0800

    完成课时添加和编辑的模态框重用

--- public/js/course-lesson.js
+++ public/js/course-lesson.js
@@ -1,4 +1,4 @@
-define(['jquery','template','util'],function($,template,util){
+define(['jquery','template','util','bootstrap'],function($,template,util){
   // 设置导航菜单选中
   util.setMenu('/course/course_add');
   // 获取课程ID
@@ -10,9 +10,35 @@ define(['jquery','template','util'],function($,template,util){
     data : {cs_id : csId},
     dataType : 'json',
     success : function(data){
+      console.log(data);
       // 解析数据，渲染页面
       var html = template('lessonTpl',data.result);
       $('#lessonInfo').html(html);
+      //处理添加课时操作
+      $('#addLesson').click(function(){
+        var html = template('modalTpl',{operate : '添加课时'});
+        $('#modalInfo').html(html);
+        $('#chapterModal').modal();
+      });
+      //处理编辑课时操作
+      $('.editLesson').click(function(){
+        // 获取课时ID
+        var ctId = $(this).attr('data-ctId');
+        $.ajax({
+          type: 'get',
+          url: '/api/course/chapter/edit',
+          data: {ct_id:ctId},
+          dataType: 'json',
+          success: function(data) {
+            console.log(data);
+            // 解析数据，渲染模态框
+            data.result.operate = '编辑课时';
+            var html = template('modalTpl',data.result);
+            $('#modalInfo').html(html);
+            $('#chapterModal').modal();
+          }
+        });
+      });
     }
   });
   
--- public/js/course-picture.js
+++ public/js/course-picture.js
@@ -25,10 +25,12 @@ define(['jquery','template','util','uploadify','jcrop','form'],function($,templa
                 fileObjName : 'cs_cover_original',
                 formData : {cs_id : csId},
                 onUploadSuccess : function(a,b){
-                    //console.log(b);
+                    console.log(b);
                     var obj = JSON.parse(b);
                     $('.preview img').attr('src',obj.result.path);
+                    //图片上传成功，默认开始裁切
                     cropImage();
+                    //事件里添加的自定义属性，刷新页面就不存在了
                     $('#cropBtn').text('保存图片').attr('data-flag',true);
                 }
             });
@@ -48,6 +50,7 @@ define(['jquery','template','util','uploadify','jcrop','form'],function($,templa
                         success : function(data){
                             console.log(data);
                             if(data.code == 200){
+                                //cropImage(); 不管跨域的报错，再点击一次按钮可以
                                 location.href = '/course/lesson?cs_id=' + data.result.cs_id;
                             }
                         }
@@ -68,34 +71,42 @@ define(['jquery','template','util','uploadify','jcrop','form'],function($,templa
                     // 销毁当前实例
                     nowCrop && nowCrop.destroy();
                     nowCrop = this;
+                    //清除已经存在的图
                     $('.thumb').html('');
                     // 显示缩略图
                     this.initComponent('Thumbnailer',{width : 240,height : 120,mythumb:'.thumb'});
-                    //console.log(this);
+                    console.log(this);
                     // 获取图片的宽度和高度
                     var width = this.ui.stage.width;
                     var height = this.ui.stage.height;
+                    console.log(width);
+                    console.log(height);
                     // 计算选区的数据
                     var x = 0;
                     var y = (height - width/2)/2;
                     var w = width;
                     var h = width/2;
+                    console.log(x,y,w,h);
                     // 创建一个选区
                     this.newSelection();
                     this.setSelect([x,y,w,h]);
-                    // 监控选区的变化
+
+                    // 监控选区的变化,这里必须放这里面，或许是因为是不同的选区吧，还是什么？
                     img.parent().on('cropstart cropmove cropend',function(a,b,c){
-                        console.log(1);
+                        //console.log(img.parent());
+                        //console.log(3);
                         // 选区完成和变化的时候把对应的坐标数据填充到表单里
-                        console.log(c);
+                        //console.log(c);
                         var aInput = $('#cropForm').find('input');
                         aInput.eq(0).val(c.x);
                         aInput.eq(1).val(c.y);
                         aInput.eq(2).val(c.w);
                         aInput.eq(3).val(c.h);
                     });
+
                 });
-                //console.log(2);
+                console.log(2);
+
             }
         }
     });
